668e8893360010fed0c317196f3c94d21394c54d
Yuhan Sun <wdmzssyh@gmail.com>
2016-02-27 18:01:45 -0700

    Univese and RMBR ini

---
+++ GeoReach/Construction/Universe.cpp
@@ -0,0 +1,204 @@
+#include "stdafx.h"
+#include "Universe.h"
+
+bool Location_In_Rect(Location m_location, MyRect m_rect)
+{
+	if (m_location.x<m_rect.left_bottom.x || m_location.x>m_rect.right_top.x || m_location.y<m_rect.left_bottom.y || m_location.y>m_rect.right_top.y)
+		return false;
+	else
+		return true;
+}
+
+void OutFile(Entity Entity_Matrix[], int node_count, string filename)
+{
+	ofstream file;
+	file.open("data/" + filename);
+	for (int i = 0; i < node_count; i++)
+	{
+		file << i << "    " << Entity_Matrix[i].IsSpatial << "    " << Entity_Matrix[i].location.x << "  " << Entity_Matrix[i].location.y << "    " << Entity_Matrix[i].type;
+		file << endl;
+	}
+	file.close();
+}
+
+void OutFile(vector<Entity> &entity_vector, string filename)
+{
+	ofstream file;
+	file.open("data/" + filename);
+	for (int i = 0; i < entity_vector.size(); i++)
+	{
+		file << i << "    " << entity_vector[i].IsSpatial << "    " << entity_vector[i].location.x << "  " << entity_vector[i].location.y << "    " << entity_vector[i].type;
+		file << endl;
+	}
+	file.close();
+}
+
+void EntityToDisk(Entity Entity_Matrix[], int node_count, int range, string filename)
+{
+	string root = "data/";
+	root += filename;
+	char *ch = (char *)root.data();
+	freopen(ch, "w", stdout);
+	
+	printf("%d %d\n", node_count,range);
+	for (int i = 0; i < node_count; i++)
+	{
+		printf("%d %d %f %f %d\n", i, Entity_Matrix[i].IsSpatial, Entity_Matrix[i].location.x, Entity_Matrix[i].location.y, Entity_Matrix[i].type);
+	}
+	fclose(stdout);
+}
+
+void EntityToDisk(vector<Entity> entity_vector, int range, string filename)
+{
+	string root = "data/";
+	root += filename;
+	char *ch = (char *)root.data();
+	freopen(ch, "w", stdout);
+
+	printf("%d %d\n", entity_vector.size(), range);
+	for (int i = 0; i < entity_vector.size(); i++)
+	{
+		printf("%d %d %f %f %d\n", i, entity_vector[i].IsSpatial, entity_vector[i].location.x, entity_vector[i].location.y, entity_vector[i].type);
+	}
+	fclose(stdout);
+}
+
+void ReadEntityFromDisk(int &node_count, Entity Entity_Matrix[], int &range, string filename)
+{
+	string root = "data/";
+	root += filename;
+	char *ch = (char *)root.data();
+	freopen(ch, "r", stdin);
+
+	scanf("%d %d", &node_count, &range);
+	for (int i = 0; i < node_count; i++)
+	{
+		scanf("%d %d %lf %lf %d", &(Entity_Matrix[i].id), &(Entity_Matrix[i].IsSpatial), &(Entity_Matrix[i].location.x), &(Entity_Matrix[i].location.y), &(Entity_Matrix[i].type));
+	}
+	fclose(stdin);
+}
+
+void ReadEntityFromDisk(int &node_count, vector<Entity> &entity_vector, int &range, string filename)
+{
+	char *ch = (char *)filename.data();
+	freopen(ch, "r", stdin);
+
+	scanf("%d %d", &node_count, &range);
+	entity_vector.resize(node_count);
+	for (int i = 0; i < node_count; i++)
+	{
+		scanf("%d %d %lf %lf %d", &(entity_vector[i].id), &(entity_vector[i].IsSpatial), &(entity_vector[i].location.x), &(entity_vector[i].location.y), &(entity_vector[i].type));
+	}
+	fclose(stdin);
+}
+
+void ReadEntityInSCCFromDisk(int &node_count, vector<Entity> &entity_vector, int &range, string filename)
+{
+	char *ch = (char *)filename.data();
+	freopen(ch, "r", stdin);
+
+	scanf("%d %d", &node_count, &range);
+	entity_vector.resize(node_count);
+	for (int i = 0; i < node_count; i++)
+	{
+		scanf("%d %d %lf %lf %d %d %lf %lf %lf %lf", &(entity_vector[i].id), &(entity_vector[i].IsSpatial), &(entity_vector[i].location.x), &(entity_vector[i].location.y), &(entity_vector[i].type), &(entity_vector[i].scc_id), &(entity_vector[i].RMBR.left_bottom.x), &(entity_vector[i].RMBR.left_bottom.y), &(entity_vector[i].RMBR.right_top.x), &(entity_vector[i].RMBR.right_top.y));
+	}
+	fclose(stdin);
+}
+
+string getstring(const int i)
+{
+	stringstream newstr;
+	newstr << i;
+	return newstr.str();
+}
+
+int StringtoInt(string str)
+{
+	return atoi(str.c_str());
+}
+
+vector<string> split(string str, string pattern)
+{
+	vector<string> ret;
+	if (pattern.empty()) return ret;
+	size_t start = 0, index = str.find_first_of(pattern, 0);
+	while (index != str.npos)
+	{
+		if (start != index)
+			ret.push_back(str.substr(start, index - start));
+		start = index + 1;
+		index = str.find_first_of(pattern, start);
+	}
+	if (!str.substr(start).empty())
+		ret.push_back(str.substr(start));
+	return ret;
+}
+
+// A recursive function used by topologicalSort
+void TopologicalSortUtil(int v, vector<bool> &visited, queue<int> &queue, vector<vector<int>> &graph)
+{
+	// Mark the current node as visited.
+	visited[v] = true;
+
+	// Recur for all the vertices adjacent to this vertex
+	for (int i = 0; i < graph[v].size(); i++)
+		if (!visited[graph[v][i]])
+			TopologicalSortUtil(graph[v][i], visited, queue, graph);
+
+	// Push current vertex to stack which stores result
+	queue.push(v);
+}
+
+// The function to do Topological Sort. It uses recursive topologicalSortUtil()
+void TopologicalSort(vector<vector<int>> &graph, queue<int> &queue)
+{
+	// Mark all the vertices as not visited
+	vector<bool> visited = vector<bool>(graph.size());
+
+	for (int i = 0; i < graph.size(); i++)
+		visited[i] = false;
+
+	// Call the recursive helper function to store Topological Sort
+	// starting from all vertices one by one
+	for (int i = 0; i < graph.size(); i++)
+		if (visited[i] == false)
+			TopologicalSortUtil(i, visited, queue, graph);
+}
+
+void ReadGraph(vector<vector<int>> &graph, int &node_count, string graph_filepath)
+{
+	char *ch = (char *)graph_filepath.data();
+	freopen(ch, "r", stdin);
+
+	scanf("%d\n", &node_count);
+	graph.resize(node_count);
+
+	for (int i = 0; i < node_count; i++)
+	{
+		int id, count, outid;
+		scanf("%d %d ", &id, &count);
+		graph[i].resize(count);
+		for (int j = 0; j < count; j++)
+		{
+			scanf("%d ", &outid);
+			graph[i][j] = outid;
+		}
+	}
+	fclose(stdin);
+}
+
+void GenerateInedgeGraph(vector<vector<int>> &graph, vector<vector<int>> &in_edge_graph)
+{
+	int node_count = graph.size();
+	in_edge_graph.resize(node_count);
+	for (int i = 0; i < node_count; i++)
+	{
+		for (int j = 0; j < graph[i].size(); j++)
+		{
+			int neighbor = graph[i][j];
+			in_edge_graph[neighbor].push_back(i);
+		}
+	}
+}
+
---
+++ GeoReach/Construction/Universe.h
@@ -0,0 +1,163 @@
+/*Universe.h*/
+#ifndef _UNIVERSE_H_
+#define _UNIVERSE_H_
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include <vector>
+#include <set>
+#include <hash_set>
+#include <hash_map>
+#include <stack>
+#include <queue>
+#include <iterator>
+#include <time.h>
+#include <sstream>
+
+/*General utilization functions*/
+string getstring(const int i);
+int StringtoInt(string str);
+vector<string> split(string str, string pattern);
+
+/*Point location*/
+struct Location
+{
+	double x, y;
+
+	Location(double a, double b)
+	{
+		x = a;
+		y = b;
+	}
+	Location()
+	{
+		x = 0;
+		y = 0;
+	}
+
+	Location copy()
+	{
+		Location p_location = Location(x, y);
+		return p_location;
+	}
+};
+
+/*Rectangle*/
+struct MyRect
+{
+	//whether it has a real rectangle or just a empty rectangle
+	boolean HasRec;
+
+	Location left_bottom;
+	Location right_top;
+
+	MyRect(double minx, double miny, double maxx, double maxy)
+	{
+		HasRec = true;
+		left_bottom.x = minx;
+		left_bottom.y = miny;
+		right_top.x = maxx;
+		right_top.y = maxy;
+	}
+
+	MyRect(Location p_left_bottom, Location p_right_top)
+	{
+		HasRec = true;
+		left_bottom = Location(p_left_bottom.x, p_left_bottom.y);
+		right_top = Location(p_right_top.x, p_right_top.y);
+	}
+
+	MyRect()
+	{
+		HasRec = false;
+		left_bottom.x = 0;
+		left_bottom.y = 0;
+		right_top.x = 0;
+		right_top.y = 0;
+	}
+
+	MyRect copy()
+	{
+		MyRect rect = MyRect(left_bottom, right_top);
+		return rect;
+	}
+
+	void MBR(MyRect end_rect)
+	{
+		//end_rect is null
+		if (!end_rect.HasRec)
+			return;
+		else
+		{
+			//current is null
+			if (!HasRec)
+			{
+				HasRec = true;
+				left_bottom = end_rect.left_bottom.copy();
+				right_top = end_rect.right_top.copy();
+			}
+			else
+			{
+				if (left_bottom.x > end_rect.left_bottom.x)
+					left_bottom.x = end_rect.left_bottom.x;
+				if (left_bottom.y > end_rect.left_bottom.y)
+					left_bottom.y = end_rect.left_bottom.y;
+				if (right_top.x < end_rect.right_top.x)
+					right_top.x = end_rect.right_top.x;
+				if (right_top.y < end_rect.right_top.y)
+					right_top.y = end_rect.right_top.y;
+			}
+		}
+	}
+
+	void MBR(Location point)
+	{
+		if (!HasRec)
+		{
+			HasRec = true;
+			left_bottom = Location(point.x, point.y);
+			right_top = Location(point.x, point.y);
+		}
+		else
+		{
+			if (left_bottom.x > point.x)
+				left_bottom.x = point.x;
+			if (left_bottom.y > point.y)
+				left_bottom.y = point.y;
+			if (right_top.x < point.x)
+				right_top.x = point.x;
+			if (right_top.y < point.y)
+				right_top.y = point.y;
+		}
+	}
+};
+
+/*Vertex Entity*/
+struct Entity
+{
+	int id;
+	bool IsSpatial;
+	Location location;
+	MyRect RMBR;
+	int type;
+	int scc_id;
+};
+
+//Check whether a location is in a specific rectangle
+bool Location_In_Rect(Location m_location, MyRect m_rect);
+
+//Read entity from disk storage
+void ReadEntityInSCCFromDisk(int &node_count, vector<Entity> &entity_vector, int &range, string filename);
+
+/*Graph related functions*/
+//Topological sort of dag
+void TopologicalSortUtil(int v, vector<bool> &visited, queue<int> &Queue, vector<vector<int>> &graph);
+void TopologicalSort(vector<vector<int>> &graph, queue<int> &Queue);
+
+/*Read Graph from adjacent list txt file*/
+void ReadGraph(vector<vector<int>> &graph, int &node_count, string graph_filepath);
+
+/*Get in_edge graph*/
+void GenerateInedgeGraph(vector<vector<int>> &graph, vector<vector<int>> &in_edge_graph);
+
+#endif
